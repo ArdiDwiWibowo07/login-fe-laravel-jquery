fc1faacc2f814f195c7c3b636fb31244af0b8998
ArdiDwiWibowo07 <ardw.wibowo@gmail.com>
2024-11-21 06:55:39 +0700

    [updated]

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('logout', [LoginController::class, 'logout']);
+Route::post('auth/login', [LoginController::class, 'login'])->name('auth.login');
